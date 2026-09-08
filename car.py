d41c84fcb63aa4d4b2202f2246787dc2e30ad735
RutujaBapat28 <rutujabapat60@gmail.com>
2026-09-08 13:15:37 +0530

    Add files via upload

---
+++ car.py
@@ -0,0 +1,190 @@
+import tkinter as tk
+import pymysql
+from tkinter import messagebox
+
+
+class car:
+    def __init__(self,root):
+        self.root=root
+        self.root.title("Car Rental Management System")
+        self.width=self.root.winfo_screenwidth()
+        self.height = self.root.winfo_screenheight()
+        self.root.geometry(f"{self.width}x{self.height}+0+0")
+        
+        title=tk.Label(self.root,text="Car Rental Management System",bg="light green",fg="red",bd=4,relief="ridge",font=("Arial",50,"bold"))
+        title.pack(side="top",fill="x")
+        
+        self.mainFrame=tk.Frame(self.root,bg="light gray",bd=4,relief="groove")
+        self.mainFrame.place(width=self.width/2.5, height=self.height-200,x=self.width/6-150,y=100)
+
+        reserveBtn = tk.Button(self.mainFrame,command=self.reserve,text="Reserve Car",bg="sky blue", fg="black", bd=5, relief="raised",width=20,font=("Arial",30))
+        reserveBtn.grid(padx=120, pady=60,row=0,column=0)
+        
+        returnBtn = tk.Button(self.mainFrame,command=self.returnFun,text="Return Car",bg="sky blue", fg="black", bd=5, relief="raised",width=20, font=("Arial",30))
+        returnBtn.grid(padx=40, pady=60,row=1,column=0)
+        
+        closeBtn = tk.Button(self.mainFrame,command=self.closeWindow,text="Close Window",bg="sky blue", fg="black", bd=5, relief="raised",width=20, font=("Arial",30))
+        closeBtn.grid(padx=40, pady=60,row=3,column=0)
+        
+        feedbBtn = tk.Button(self.mainFrame,command=self.feedback,text="FeedBack",bg="sky blue", fg="black", bd=5, relief="raised",width=20, font=("Arial",30))
+        feedbBtn.grid(padx=40, pady=60,row=2,column=0)
+        
+        self.showFrame= tk.Frame(self.root,bg="light gray",bd=4,relief="groove")
+        self.showFrame.place(width=self.width/3,height=self.height-200,x=self.width/2+50,y=100)
+        
+        lab= tk.Label(self.showFrame,text="Available Cars",bg="light gray",fg="red",font=("Arial",30,"bold"))
+        lab.grid(row=0,column=1,padx=50,pady=20)
+        
+        self.list= tk.Listbox(self.showFrame,fg="white",font=("Arial",15,"bold"),bg="gray",bd=4,relief="sunken",width=53, height=20)
+        self.list.grid(row=1,column=1,padx=10,pady=50,columnspan=2)
+        self.retrieve()
+        
+    def db_conn(self):                         
+        self.con = pymysql.connect(host="localhost", user="root", passwd="VRUSH@#2314", database="cars")
+        self.cur = self.con.cursor()   
+        
+    def retrieve(self):
+        self.db_conn()
+        self.cur.execute("select * from car where status='Avail'")
+        data = self.cur.fetchall()
+        
+        for i in data:
+            line = ' : '.join(map(str,i))
+            self.list.insert(tk.END,line)
+            
+                                      
+    def closeWindow(self):
+        self.root.destroy()   
+        
+    def reserve(self):
+        self.resFrame= tk.Frame(self.root,bg="sky blue",bd=4,relief="groove")
+        self.resFrame.place(width=self.width/3,height=self.height-180,x=self.width/2+50,y=100)
+        
+        regLab = tk.Label(self.resFrame,text="Car No: ", bg="sky blue",fg="black", font=("Arial",15,"bold"))
+        regLab.grid(row=0,column=0, padx=20, pady=50)
+        self.regIn= tk.Entry(self.resFrame,bd=2, font=("Arial",15),width=20)
+        self.regIn.grid(row=0, column=1, padx=20, pady=50)
+        
+        dayLab = tk.Label(self.resFrame,text="Enter Days: ", bg="sky blue",fg="black", font=("Arial",15,"bold"))
+        dayLab.grid(row=1, column=0, padx=20, pady=50)
+        self.dayIn = tk.Entry(self.resFrame,bd=2, font=("Arial",15),width=20)
+        self.dayIn.grid(row=1,column=1,padx=20, pady=50)
+        
+        okBtn= tk.Button(self.resFrame, text="OK",command=self.resFun, font=("Arial",20),width=20, bg="light gray", bd=2)
+        okBtn.grid(row=2,column=0,padx=30, pady=60,columnspan=2)
+    
+    def resFun(self): 
+        
+        reg = int(self.regIn.get())
+        days= int(self.dayIn.get())
+
+        self.db_conn()
+        try: 
+            query = f"select rent, status from car where regNo={reg}"
+            self.cur.execute(query)
+            row= self.cur.fetchone()
+            if row[1] == 'Avail':
+                query2= f"update car set status='Reserved' where regNo={reg}"
+                self.cur.execute(query2)
+                self.con.commit()
+                amount = days * row[0]
+                bill= f"Amount for {reg} is: {amount}"
+                tk.messagebox.showinfo("Success",bill)
+                self.cur.execute("select * from car where status='Avail'")
+                values = self.cur.fetchall()
+                self.list.delete(0,tk.END)
+                for j in values:
+                    newVal= ' : '.join(map(str,j))
+                    self.list.insert(tk.END,newVal)
+                self.closeRes()
+                
+            else:
+                tk.messagebox.showerror("Error","Car is not available") 
+                self.closeRes()
+        except Exception as e:
+            tk.messagebox.showerror("Error",f"DB Error: {e}")  
+            
+            
+    def closeRes(self):
+        self.resFrame.destroy() 
+    
+    def returnFun(self):
+        self.retFrame= tk.Frame(self.root,bg="sky blue",bd=4,relief="groove")
+        self.retFrame.place(width=self.width/3,height=self.height-180,x=self.width/2+50,y=100)   
+
+        regLab = tk.Label(self.retFrame,text="Car No: ", bg="sky blue",fg="black", font=("Arial",15,"bold"))
+        regLab.grid(row=0,column=0, padx=20, pady=50)
+        self.regIn2= tk.Entry(self.retFrame,bd=2, font=("Arial",15),width=20)
+        self.regIn2.grid(row=0, column=1, padx=20, pady=50)
+
+        okBtn= tk.Button(self.retFrame, text="OK",command=self.retFun, font=("Arial",20),width=20, bg="light gray", bd=2)
+        okBtn.grid(row=2,column=0,padx=30, pady=60,columnspan=2)
+
+    def retFun(self):
+        reg=int(self.regIn2.get())
+
+        self.db_conn()
+        query=f"update car set status='Avail' where regNo={reg}"
+        self.cur.execute(query)
+        self.con.commit()
+
+        self.cur.execute("select * from car where status='Avail'")
+        data = self.cur.fetchall()
+        self.list.delete(0,tk.END)
+        for i in data:
+            newVal=' : '.join(map(str,i))
+            self.list.insert(tk.END,newVal)
+        tk.messagebox.showinfo("Sucess","Car Returned Successfuly")
+        self.cur.close()
+        self.con.close()
+        self.closeRet()
+        
+    def closeRet(self):
+       self.retFrame.destroy()
+        
+    def feedback(self):
+        self.feedFrame= tk.Frame(self.root,bg="sky blue",bd=4,relief="groove")
+        self.feedFrame.place(width=self.width/3,height=self.height-180,x=self.width/2+50,y=100)
+        
+        name = tk.Label(self.feedFrame,text="Name: ", bg="sky blue",fg="black", font=("Arial",15,"bold"))
+        name.grid(row=0,column=0, padx=20, pady=50)
+        self.name_entry= tk.Entry(self.feedFrame,bd=2, font=("Arial",15),width=30)
+        self.name_entry.grid(row=0, column=1, padx=20, pady=50)
+        
+        email = tk.Label(self.feedFrame,text="Email: ", bg="sky blue",fg="black", font=("Arial",15,"bold"))
+        email.grid(row=1, column=0, padx=20, pady=50)
+        self.email1 = tk.Entry(self.feedFrame,bd=2, font=("Arial",15),width=30)
+        self.email1.grid(row=1,column=1,padx=20, pady=50)
+
+        text1 = tk.Label(self.feedFrame,text="Feedback: ", bg="sky blue",fg="black", font=("Arial",15,"bold"))
+        text1.grid(row=2, column=0, padx=20, pady=50)
+        self.text2 = tk.Text(self.feedFrame,bd=2, font=("Arial",15),width=30,height=3)
+        self.text2.grid(row=2,column=1,padx=20, pady=20)
+        
+        okBtn= tk.Button(self.feedFrame, text="OK",command=self.feedfun, font=("Arial",20),width=20, bg="light gray", bd=2)
+        okBtn.grid(row=3,column=0,padx=30, pady=60,columnspan=2)
+    def feedfun(self):
+        name = self.name_entry.get()
+        email = self.email1.get()
+        feedback = self.text2.get("1.0", tk.END).strip()
+
+        if name == "" or email == "" or feedback == "":
+            messagebox.showerror("Error", "All fields are required")
+            return
+        self.db_conn()
+        query = "INSERT INTO feedback (name, email, message) VALUES (%s, %s, %s)"
+        values = (name, email, feedback)
+        self.cur.execute(query, values)
+        self.con.commit()
+
+        messagebox.showinfo("Success", "Thank you for your feedback!")
+        self.cur.close()
+        self.con.close()
+        self.closefe()
+        
+    def closefe(self):
+       self.feedFrame.destroy()
+
+root=tk.Tk()
+obj=car(root)
+root.mainloop() 
